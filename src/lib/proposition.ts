27b1cdd7aa4e3f72c28b9fabc7c309bc7707245c
Patrick Coffey <patrickcoffey48@gmail.com>
2019-07-19 17:11:01 -0500

    feat: add tslint and prettier tooling

--- src/lib/proposition.ts
+++ src/lib/proposition.ts
@@ -1,30 +1,44 @@
-import { getTraversal } from './util';
 import gremlin from 'gremlin';
+import { getTraversal } from './util';
 
-type NewProposition = {
-  text: string,
-  supports: Array<number>,
-  supportedBy: Array<number>,
+interface INewProposition {
+  text: string;
+  supports: number[];
+  supportedBy: number[];
 }
-export type Proposition = {
-  id: number,
-  text: string,
-  supports: Array<number>,
-  supportedBy: Array<number>,
-};
+export interface IProposition {
+  id: number;
+  text: string;
+  supports: number[];
+  supportedBy: number[];
+}
 
 const __ = gremlin.process.statics;
 
-export async function createProposition(input: NewProposition): Promise<Proposition> {
+export async function createProposition(
+  input: INewProposition
+): Promise<IProposition> {
   let g = getTraversal();
-  g = g.addV('proposition').as('prop').property('text', input.text);
-
+  g = g
+    .addV('proposition')
+    .as('prop')
+    .property('text', input.text);
 
   if (input.supports.length) {
-    g = g.V(...input.supports).as('s').addE('supports').from_('prop').to('s');
+    g = g
+      .V(...input.supports)
+      .as('s')
+      .addE('supports')
+      .from_('prop')
+      .to('s');
   }
   if (input.supportedBy.length) {
-    g = g.V(...input.supportedBy).as('sb').addE('supports').from_('sb').to('prop');
+    g = g
+      .V(...input.supportedBy)
+      .as('sb')
+      .addE('supports')
+      .from_('sb')
+      .to('prop');
   }
 
   return (await g
@@ -32,34 +46,61 @@ export async function createProposition(input: NewProposition): Promise<Proposit
     .project('id', 'text', 'supports', 'supportedBy')
     .by(__.id())
     .by(__.values('text'))
-    .by(__.out('supports').id().fold())
-    .by(__.in_('supports').id().fold())
+    .by(
+      __.out('supports')
+        .id()
+        .fold()
+    )
+    .by(
+      __.in_('supports')
+        .id()
+        .fold()
+    )
     .next()).value;
 }
 
-export async function deleteProposition(id: NewProposition): Promise<void> {
-  await getTraversal().V(id).drop().next();
+export async function deleteProposition(id: INewProposition): Promise<void> {
+  await getTraversal()
+    .V(id)
+    .drop()
+    .next();
 }
 
-export async function getProposition(id: string): Promise<Proposition> {
+export async function getProposition(id: string): Promise<IProposition> {
   return (await getTraversal()
     .V(id)
     .project('id', 'text', 'supports', 'supportedBy')
     .by(__.id())
     .by(__.values('text'))
-    .by(__.out('supports').id().fold())
-    .by(__.in_('supports').id().fold())
+    .by(
+      __.out('supports')
+        .id()
+        .fold()
+    )
+    .by(
+      __.in_('supports')
+        .id()
+        .fold()
+    )
     .next()).value;
 }
 
-export async function listPropositions(): Promise<Array<Proposition>> {
+export async function listPropositions(): Promise<IProposition[]> {
   return (await getTraversal()
     .V()
     .hasLabel('proposition')
     .project('id', 'text', 'supports', 'supportedBy')
     .by(__.id())
     .by(__.values('text'))
-    .by(__.out('supports').id().fold())
-    .by(__.in_('supports').id().fold())
+    .by(
+      __.out('supports')
+        .id()
+        .fold()
+    )
+    .by(
+      __.in_('supports')
+        .id()
+        .fold()
+    )
     .next()).value;
 }
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,9 +1,9 @@
-import gremlin from "gremlin";
+import gremlin from 'gremlin';
 const traversal = gremlin.process.AnonymousTraversalSource.traversal;
 const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection;
 
 export function getTraversal() {
   return traversal().withRemote(
-    new DriverRemoteConnection("ws://localhost:3182/gremlin", {})
+    new DriverRemoteConnection('ws://localhost:3182/gremlin', {})
   );
 }
